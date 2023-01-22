c57448124a4331a50101eaafc5620c45e7d93c69
Shreyans Jain <shreyans@duelance.app>
2023-01-22 14:21:41 +0530

    Add vercel analytics
    Signed-off-by: Shreyans Jain <shreyans@duelance.app>

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,8 @@
 /****************************************************************************
  * Copyright 2023 Duelance Inc. - All Rights Reserved
- * 
+ *
  * This file is part of the Duelance project.
- * 
+ *
  * Unauthorized copying, transferring or reproduction of this file, via any medium is strictly prohibited.
  * Proprietary and confidential.
  ****************************************************************************/
@@ -13,7 +13,8 @@ import { type AppType } from "next/app"
 import { type Session } from "next-auth"
 import { SessionProvider } from "next-auth/react"
 import { Provider } from "jotai"
-import {api} from "@/utils/api"
+import { api } from "@/utils/api"
+import { Analytics } from "@vercel/analytics/react"
 
 const inter = Inter({ subsets: ["latin"] })
 
@@ -27,6 +28,7 @@ const MyApp: AppType<{ session: Session | null }> = ({
                 <div className={inter.className}>
                     <Component {...pageProps} />
                 </div>
+                <Analytics />
             </SessionProvider>
         </Provider>
     )
