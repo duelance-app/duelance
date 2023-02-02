f44fe498a8bf9258cd383246b43adc2678df27ab
Shreyans Jain <shreyans@duelance.app>
2023-02-02 23:06:46 +0530

    fix: remove tailwindui
    Signed-off-by: Shreyans Jain <shreyans@duelance.app>

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -8,14 +8,9 @@
  *********************************************************************************************************/
 
 import { type NextPage } from "next"
-import Header from "@/components/Header"
 
 const Home: NextPage = () => {
-    return (
-        <>
-            <Header />
-        </>
-    )
+    return <></>
 }
 
 export default Home
