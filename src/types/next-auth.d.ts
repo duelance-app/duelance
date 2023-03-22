a58846d4402c3b5a98352588ac5b15785799ce57
Shreyans Jain <shreyans@duelance.app>
2023-03-22 18:11:59 +0530

    feat: add ts-reset
    Signed-off-by: Shreyans Jain <shreyans@duelance.app>

--- src/types/next-auth.d.ts
+++ src/types/next-auth.d.ts
@@ -9,6 +9,8 @@
 
 import { type DefaultSession } from "next-auth"
 
+import "@total-typescript/ts-reset"
+
 declare module "next-auth" {
     /**
      * Returned by `useSession`, `getSession` and received as a prop on the `SessionProvider` React Context
